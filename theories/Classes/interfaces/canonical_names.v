9a60842ac0bbf27826e7b6cd5eac970647d46f55
Ali Caglayan <alizter@gmail.com>
2024-12-23 02:46:46 +0000

    remove (+), (_ +), (+ _) from mc_add_scope
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- theories/Classes/interfaces/canonical_names.v
+++ theories/Classes/interfaces/canonical_names.v
@@ -125,9 +125,6 @@ Module Export BinOpNotations.
 (* Notations: *)
 Declare Scope mc_add_scope.
 Infix "+" := sg_op : mc_add_scope.
-Notation "(+)" := sg_op (only parsing) : mc_add_scope.
-Notation "( x +)" := (sg_op x) (only parsing) : mc_add_scope.
-Notation "(+ x )" := (fun y => sg_op y x) (only parsing) : mc_add_scope.
 
 Declare Scope mc_mult_scope.
 Infix "*" := sg_op : mc_mult_scope.
